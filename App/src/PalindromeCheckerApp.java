8a818dfcc15603989de5c45d12673cf65006c139
nishantkumar73 <nishantk9122@gmail.com>
2026-02-23 15:16:34 +0530

    Kushagra Chandra added UC3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,12 +1,13 @@
 public class PalindromeCheckerApp {
     public static void main(String args[]) {
+        //Checking palindrome by reversing he string and then comparing
         String word="madam";
-        boolean ans=true;
-        for(int i=0;i<word.length()/2;i++){
-            if(word.charAt(i)!=word.charAt(word.length()-i-1)) ans=false;
+        String reverse="";
+        for(int i=word.length()-1;i>=0;i--) {
+            reverse += word.charAt(i);
         }
         System.out.println("Input Text: "+word);
-        System.out.println("Is it a Palindrome: "+ans);
+        System.out.println("Is it a Palindrome: "+word.equals(reverse));
 
     }
 }
