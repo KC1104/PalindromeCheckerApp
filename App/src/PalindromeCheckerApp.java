6d39a03ae8f05aabbe4b8e23f0f6cf89360ccf1d
nishantkumar73 <nishantk9122@gmail.com>
2026-02-23 14:34:30 +0530

    Kushagra Chandra added UC1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp {
+    public static void main(String args[]) {
+        System.out.print("Welcome to My Palindrome Checker Management System\nVersion: 1.0\nSystem initialised successfully");
+
+    }
+}
