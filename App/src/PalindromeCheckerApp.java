f184f361c239c066d17c2d2c8925fef6da210018
gamerProg <gamer.dan444@gmail.com>
2026-03-10 14:24:34 +0530

    Kushagra Chandra added UC8

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String args[]) {
         //Checking palindrome by using Queue and Stack
-        String word="refer";
+        String word="level";
         boolean ans=true;
-        Deque<Character> d=new ArrayDeque<>();
+        LinkedList<Character> l=new LinkedList<>();
         for(int i=0;i<word.length();i++){
-            d.add(word.charAt(i));
+            l.add(word.charAt(i));
         }
-        while(d.size()>1){
-            if(d.pop()!=d.removeLast()) ans=false;
+        while(l.size()>1){
+            if(l.removeFirst()!=l.removeLast()) ans=false;
         }
         System.out.println("Input Text: "+word);
         System.out.println("Is it a Palindrome: "+ans);
