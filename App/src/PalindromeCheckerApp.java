971b85117ffb573cf9b78efe107c2258eaa9e12c
gamerProg <gamer.dan444@gmail.com>
2026-03-10 15:24:06 +0530

    Kushagra Chandra added UC10

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 public class PalindromeCheckerApp {
-    public static boolean check(int f,int l,String w){
-        if(w.charAt(f)!=w.charAt(l)) return false;
-        while(f<=l){
-            f++;l--;
-            check(f,l,w);
-        }
-        return true;
-
-    }
     public static void main(String args[]) {
         //Checking palindrome by using Queue and Stack
-        String word="madam";
-        boolean ans=check(0,word.length()-1,word);
+        String word="A man a plan a canal Panama";
+        boolean ans=true;
+        String normalized="";
+        for(int i=0;i<word.length();i++){
+            if(word.charAt(i)==' ')  continue;
+            normalized+=Character.toLowerCase(word.charAt(i));
+        }
+
+        for(int i=0;i<normalized.length()/2;i++){
+            if(normalized.charAt(i)!=normalized.charAt(normalized.length()-i-1)) ans=false;
+        }
         System.out.println("Input Text: "+word);
         System.out.println("Is it a Palindrome: "+ans);
 
